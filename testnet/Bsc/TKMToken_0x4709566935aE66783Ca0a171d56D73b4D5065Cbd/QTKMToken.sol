2f168410f0610c3c557122f6f88b1e4e0f59f84b
tinyping <tinyping@prunebomb.com>
2022-08-24 10:48:35 +0900

    bsc testnet

---
+++ testnet/Bsc/TKMToken_0x4709566935aE66783Ca0a171d56D73b4D5065Cbd/QTKMToken.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.14;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "./Owner.sol";
+
+contract QTKMToken is ERC20, Owner {
+    event Ico(address indexed from);
+
+    uint256 constant _initial_supply = 1 * 1e8; // 초기 발행 수량. 1억개
+
+    constructor() ERC20("Three Kingdom Multiverse", "3KM_A") {
+        _mint(msg.sender, _initial_supply * (10**uint256(decimals())));
+    }
+}
