6118171e0f825f4b985abbe5edc1e5c9284ceffa
GeonhoLee <leesb86@prunebomb.com>
2023-01-27 16:32:13 +0900

    TKMStaking2B

--- beta/baobab/TKMStaking2/TKMStaking2B.sol
+++ beta/baobab/TKMStaking2/TKMStaking2B.sol
@@ -11,8 +11,8 @@ contract TKMStaking2B is Pausable, AccessControl {
 
     uint8 public constant gapIndex = 9; // TKMStaking1,2와의 인덱스 보정용
 
-    // stakeId (9 ~ 12: 3km, 13 ~ 16: dtc)
-    uint256[8] public stakeIdList = [9, 10, 11, 12, 13, 14, 15, 16];
+    // stakeId (9 ~ 12: 3km)
+    uint256[4] public stakeIdList = [9, 10, 11, 12];
     mapping(uint256 => bool) public stakeIds;
 
     // 스테이킹 인덱스
@@ -24,14 +24,14 @@ contract TKMStaking2B is Pausable, AccessControl {
     }
     
     // 스테이킹별 카운트
-    stakedInfo[8] public stakedInfoList;
+    stakedInfo[4] public stakedInfoList;
 
     // 스테이킹 참여(참여 지갑, 블록 넘버, stakeId, 스테이킹 인덱스, nft id 리스트)
     event Stake(address indexed from, uint256 blockNumber, uint256 stakeId, uint256 stakingIdx, uint256[] ids);
     // 스테이킹 회수
-    event Withdraw(address indexed from, uint256 blockNumber, uint256 stakingIdx);
+    event Withdraw(address indexed from, uint256 blockNumber, uint256 stakeId, uint256 stakingIdx);
     // 스테이킹 보상
-    event Harvest(address indexed from, uint256 blockNumber, uint256 stakingIdx);
+    event Harvest(address indexed from, uint256 blockNumber, uint256 stakeId, uint256 stakingIdx);
 
     /////////////////////////////////////////////////////
     // Staking
@@ -39,7 +39,8 @@ contract TKMStaking2B is Pausable, AccessControl {
     struct staking {
         uint256 startTime; // 시작 시간
         uint256 endTime; // 종료 시간
-        uint8 nftAmount; // 1회 스테이킹 가능한 NFT 수량(3개)
+        uint8 nftAmount; // 1회 스테이킹 가능한 NFT 수량(1, 3, 5, 7)
+        uint256 withdrawTime; // 회수 가능한 시간
     }
 
     // stake id별 스테이킹
@@ -85,13 +86,15 @@ contract TKMStaking2B is Pausable, AccessControl {
         uint256 _stakeId,
         uint256 _startTime,
         uint256 _endTime,
-        uint8 _nftAmount
+        uint8 _nftAmount,
+        uint256 _withdrawTime
     ) public onlyRole(DEFAULT_ADMIN_ROLE) {
-        // 종료 시간만 변경 가능
+        // 종료 시간, 회수 시간 변경 가능
         if(0 != stakings[_stakeId].startTime) {
             require(_endTime > stakings[_stakeId].startTime, "[TKMStaking2B][setStaking]: _endTime is under _startTime");
             require(_endTime > block.timestamp, "[TKMStaking2B][setStaking]: _endTime is under now");
             stakings[_stakeId].endTime = _endTime;
+            stakings[_stakeId].withdrawTime = _withdrawTime;
             return;
         }
 
@@ -102,6 +105,7 @@ contract TKMStaking2B is Pausable, AccessControl {
         newStake.startTime = _startTime;
         newStake.endTime = _endTime;
         newStake.nftAmount = _nftAmount;
+        newStake.withdrawTime = _withdrawTime;
     }
 
     // 스테이킹 참여
@@ -134,7 +138,7 @@ contract TKMStaking2B is Pausable, AccessControl {
         progress.ids = _ids;
 
         // stakedInfo 갱신
-        stakedInfoList[_stakeId - gapIndex].nftCount += 3;
+        stakedInfoList[_stakeId - gapIndex].nftCount += stakings[_stakeId].nftAmount;
         stakedInfoList[_stakeId - gapIndex].walletCount += 1;
 
         emit Stake(msg.sender, block.number, _stakeId, stakingIdx, _ids);
@@ -142,16 +146,20 @@ contract TKMStaking2B is Pausable, AccessControl {
 
     // 회수
     function withdraw(uint256 _stakingIdx) public whenNotPaused {
+        // 회수 가능 날짜 체크
+        require(block.timestamp > stakings[progressing[_stakingIdx].stakeId].withdrawTime, "[TKMStaking2B][withdraw]: withdraw time is still left");
+
         // 스테이킹 존재 및 지갑 주소 체크
         require(progressing[_stakingIdx].user != address(0), "[TKMStaking2B][withdraw]: not exist staking data");
         require(progressing[_stakingIdx].user == msg.sender, "[TKMStaking2B][withdraw]: not Owner");
 
         // stakedInfo 갱신
-        stakedInfoList[progressing[_stakingIdx].stakeId - gapIndex].nftCount -= 3;
+        stakedInfoList[progressing[_stakingIdx].stakeId - gapIndex].nftCount -= stakings[progressing[_stakingIdx].stakeId].nftAmount;
         stakedInfoList[progressing[_stakingIdx].stakeId - gapIndex].walletCount -= 1;
 
         // 기록 제거
         uint256[] memory ids = progressing[_stakingIdx].ids;
+        uint256 stakeId = progressing[_stakingIdx].stakeId;
         delete progressing[_stakingIdx];
 
         // nft 돌려주기
@@ -159,7 +167,7 @@ contract TKMStaking2B is Pausable, AccessControl {
             TKMNft.safeTransferFrom(NftHolder, msg.sender, ids[i]);
         }
 
-        emit Withdraw(msg.sender, block.number, _stakingIdx);
+        emit Withdraw(msg.sender, block.number, stakeId, _stakingIdx);
     }
 
     // harvest
@@ -168,14 +176,14 @@ contract TKMStaking2B is Pausable, AccessControl {
         require(progressing[_stakingIdx].user != address(0), "[TKMStaking2B][harvest]: not exist staking data");
         require(progressing[_stakingIdx].user == msg.sender, "[TKMStaking2B][harvest]: not Owner");
 
-        emit Harvest(msg.sender, block.number, _stakingIdx);
+        emit Harvest(msg.sender, block.number, progressing[_stakingIdx].stakeId, _stakingIdx);
     }
 
     function getIds(uint256 _stakingIdx) public view returns (uint256[] memory ids) {
         return progressing[_stakingIdx].ids;
     }
 
-    function getStakedInfo() public view returns (uint256 blockNumber, stakedInfo[8] memory stakedList) {
+    function getStakedInfo() public view returns (uint256 blockNumber, stakedInfo[4] memory stakedList) {
         return (block.number, stakedInfoList);
     }
 }
